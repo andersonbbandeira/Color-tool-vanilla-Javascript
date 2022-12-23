7f66805eba2edffb9ac2ded072d10154a9d143c9
andersonbbandeira <andersonfabmoraes@gmail.com>
2022-12-22 21:38:11 -0300

    check valid hex color

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //javascript
+
+//check to see whether the input from the user is a valid
+//hex color
+
+//1. #000000 or 000000
+//2. check the length - should be either 3 or 6 characters
+
+const isValidHex = (hex) => {
+    if(!hex) return false;
+
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
+
+console.log(isValidHex("00000"))
