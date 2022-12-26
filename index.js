889cb6acfb3d07b80623f3957e22fdf6b869f818
andersonbbandeira <andersonfabmoraes@gmail.com>
2022-12-26 17:07:25 -0300

    alter color by percentage

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ const isValidHex = (hex) => {
 const convertHexToRGB = (hex) => {
   if(!isValidHex(hex)) return null;
   
-  let strippedHex = hex.replace('#','');
-  
-  if(strippedHex.length === 3) {
-    strippedHex = strippedHex[0] + strippedHex[0]
-    + strippedHex[1] + strippedHex[1]
+  let strippedHex = hex.replace('#', '');
+
+  if(strippedHex.length === 3){
+    strippedHex = strippedHex[0] + strippedHex[0] 
+    + strippedHex[1] + strippedHex[1] 
     + strippedHex[2] + strippedHex[2];
   }
   
@@ -35,20 +35,30 @@ const convertHexToRGB = (hex) => {
   const g  = parseInt(strippedHex.substring(2,4), 16);
   const b  = parseInt(strippedHex.substring(4,6), 16);
   
-  return {r,g,b}
+  return {r,g,b};
 }
 
 const convertRGBToHex = (r,g,b) => {
   const firstPair = ("0" + r.toString(16)).slice(-2);
   const secondPair = ("0" + g.toString(16)).slice(-2);
   const thirdPair = ("0" + b.toString(16)).slice(-2);
-
-  const hex = "#" + firstPair + secondPair +thirdPair;
+  
+  const hex = "#" + firstPair + secondPair + thirdPair;
   return hex;
+}
+
+const alterColor = (hex, percentage) => {
+  const {r,g,b} = convertHexToRGB(hex);
+  
+  const amount = Math.floor((percentage/100) * 255);
+  
+  const newR = r + amount;
+  const newG = g + amount;
+  const newB = b + amount;
+  return convertRGBToHex(newR, newG, newB);
 }
 
 slider.addEventListener('input', () => {
   sliderText.textContent = `${slider.value}%`;
-
 })
 
